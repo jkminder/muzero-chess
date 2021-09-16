84115ef1801e9a7bd5baf7bd6fa330236de18a06
Julian Minder <julian.minder@icloud.com>
2021-09-16 16:05:53 +0200

    deleted not needed code, fullyconnected net not needed, also no scale of value and reward since chess reward is 1 max (value as well), removed one_hot_encoding of action

--- models.py
+++ models.py
@@ -6,39 +6,22 @@ import torch
 
 class MuZeroNetwork:
     def __new__(cls, config):
-        if config.network == "fullyconnected":
-            return MuZeroFullyConnectedNetwork(
-                config.observation_shape,
-                config.stacked_observations,
-                len(config.action_space),
-                config.encoding_size,
-                config.fc_reward_layers,
-                config.fc_value_layers,
-                config.fc_policy_layers,
-                config.fc_representation_layers,
-                config.fc_dynamics_layers,
-                config.support_size,
-            )
-        elif config.network == "resnet":
-            return MuZeroResidualNetwork(
-                config.observation_shape,
-                config.stacked_observations,
-                len(config.action_space),
-                config.blocks,
-                config.channels,
-                config.reduced_channels_reward,
-                config.reduced_channels_value,
-                config.reduced_channels_policy,
-                config.resnet_fc_reward_layers,
-                config.resnet_fc_value_layers,
-                config.resnet_fc_policy_layers,
-                config.support_size,
-                config.downsample,
-            )
-        else:
-            raise NotImplementedError(
-                'The network parameter should be "fullyconnected" or "resnet".'
-            )
+        return MuZeroResidualNetwork(
+            config.observation_shape,
+            config.stacked_observations,
+            len(config.action_space),
+            config.blocks,
+            config.channels,
+            config.reduced_channels_reward,
+            config.reduced_channels_value,
+            config.reduced_channels_policy,
+            config.resnet_fc_reward_layers,
+            config.resnet_fc_value_layers,
+            config.resnet_fc_policy_layers,
+            config.support_size,
+            config.downsample,
+        )
+    
 
 
 def dict_to_cpu(dictionary):
@@ -74,132 +57,6 @@ class AbstractNetwork(ABC, torch.nn.Module):
 
 
 ##################################
-######## Fully Connected #########
-
-
-class MuZeroFullyConnectedNetwork(AbstractNetwork):
-    def __init__(
-        self,
-        observation_shape,
-        stacked_observations,
-        action_space_size,
-        encoding_size,
-        fc_reward_layers,
-        fc_value_layers,
-        fc_policy_layers,
-        fc_representation_layers,
-        fc_dynamics_layers,
-        support_size,
-    ):
-        super().__init__()
-        self.action_space_size = action_space_size
-        self.full_support_size = 2 * support_size + 1
-
-        self.representation_network = torch.nn.DataParallel(
-            mlp(
-                observation_shape[0]
-                * observation_shape[1]
-                * observation_shape[2]
-                * (stacked_observations + 1)
-                + stacked_observations * observation_shape[1] * observation_shape[2],
-                fc_representation_layers,
-                encoding_size,
-            )
-        )
-
-        self.dynamics_encoded_state_network = torch.nn.DataParallel(
-            mlp(
-                encoding_size + self.action_space_size,
-                fc_dynamics_layers,
-                encoding_size,
-            )
-        )
-        self.dynamics_reward_network = torch.nn.DataParallel(
-            mlp(encoding_size, fc_reward_layers, self.full_support_size)
-        )
-
-        self.prediction_policy_network = torch.nn.DataParallel(
-            mlp(encoding_size, fc_policy_layers, self.action_space_size)
-        )
-        self.prediction_value_network = torch.nn.DataParallel(
-            mlp(encoding_size, fc_value_layers, self.full_support_size)
-        )
-
-    def prediction(self, encoded_state):
-        policy_logits = self.prediction_policy_network(encoded_state)
-        value = self.prediction_value_network(encoded_state)
-        return policy_logits, value
-
-    def representation(self, observation):
-        encoded_state = self.representation_network(
-            observation.view(observation.shape[0], -1)
-        )
-        # Scale encoded state between [0, 1] (See appendix paper Training)
-        min_encoded_state = encoded_state.min(1, keepdim=True)[0]
-        max_encoded_state = encoded_state.max(1, keepdim=True)[0]
-        scale_encoded_state = max_encoded_state - min_encoded_state
-        scale_encoded_state[scale_encoded_state < 1e-5] += 1e-5
-        encoded_state_normalized = (
-            encoded_state - min_encoded_state
-        ) / scale_encoded_state
-        return encoded_state_normalized
-
-    def dynamics(self, encoded_state, action):
-        # Stack encoded_state with a game specific one hot encoded action (See paper appendix Network Architecture)
-        action_one_hot = (
-            torch.zeros((action.shape[0], self.action_space_size))
-            .to(action.device)
-            .float()
-        )
-        action_one_hot.scatter_(1, action.long(), 1.0)
-        x = torch.cat((encoded_state, action_one_hot), dim=1)
-
-        next_encoded_state = self.dynamics_encoded_state_network(x)
-
-        reward = self.dynamics_reward_network(next_encoded_state)
-
-        # Scale encoded state between [0, 1] (See paper appendix Training)
-        min_next_encoded_state = next_encoded_state.min(1, keepdim=True)[0]
-        max_next_encoded_state = next_encoded_state.max(1, keepdim=True)[0]
-        scale_next_encoded_state = max_next_encoded_state - min_next_encoded_state
-        scale_next_encoded_state[scale_next_encoded_state < 1e-5] += 1e-5
-        next_encoded_state_normalized = (
-            next_encoded_state - min_next_encoded_state
-        ) / scale_next_encoded_state
-
-        return next_encoded_state_normalized, reward
-
-    def initial_inference(self, observation):
-        encoded_state = self.representation(observation)
-        policy_logits, value = self.prediction(encoded_state)
-        # reward equal to 0 for consistency
-        reward = torch.log(
-            (
-                torch.zeros(1, self.full_support_size)
-                .scatter(1, torch.tensor([[self.full_support_size // 2]]).long(), 1.0)
-                .repeat(len(observation), 1)
-                .to(observation.device)
-            )
-        )
-
-        return (
-            value,
-            reward,
-            policy_logits,
-            encoded_state,
-        )
-
-    def recurrent_inference(self, encoded_state, action):
-        next_encoded_state, reward = self.dynamics(encoded_state, action)
-        policy_logits, value = self.prediction(next_encoded_state)
-        return value, reward, policy_logits, next_encoded_state
-
-
-###### End Fully Connected #######
-##################################
-
-
-##################################
 ############# ResNet #############
 
 
@@ -229,74 +86,6 @@ class ResidualBlock(torch.nn.Module):
         return out
 
 
-# Downsample observations before representation network (See paper appendix Network Architecture)
-class DownSample(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.conv1 = torch.nn.Conv2d(
-            in_channels,
-            out_channels // 2,
-            kernel_size=3,
-            stride=2,
-            padding=1,
-            bias=False,
-        )
-        self.resblocks1 = torch.nn.ModuleList(
-            [ResidualBlock(out_channels // 2) for _ in range(2)]
-        )
-        self.conv2 = torch.nn.Conv2d(
-            out_channels // 2,
-            out_channels,
-            kernel_size=3,
-            stride=2,
-            padding=1,
-            bias=False,
-        )
-        self.resblocks2 = torch.nn.ModuleList(
-            [ResidualBlock(out_channels) for _ in range(3)]
-        )
-        self.pooling1 = torch.nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
-        self.resblocks3 = torch.nn.ModuleList(
-            [ResidualBlock(out_channels) for _ in range(3)]
-        )
-        self.pooling2 = torch.nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        for block in self.resblocks1:
-            x = block(x)
-        x = self.conv2(x)
-        for block in self.resblocks2:
-            x = block(x)
-        x = self.pooling1(x)
-        for block in self.resblocks3:
-            x = block(x)
-        x = self.pooling2(x)
-        return x
-
-
-class DownsampleCNN(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, h_w):
-        super().__init__()
-        mid_channels = (in_channels + out_channels) // 2
-        self.features = torch.nn.Sequential(
-            torch.nn.Conv2d(
-                in_channels, mid_channels, kernel_size=h_w[0] * 2, stride=4, padding=2
-            ),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.MaxPool2d(kernel_size=3, stride=2),
-            torch.nn.Conv2d(mid_channels, out_channels, kernel_size=5, padding=2),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.avgpool = torch.nn.AdaptiveAvgPool2d(h_w)
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.avgpool(x)
-        return x
-
-
 class RepresentationNetwork(torch.nn.Module):
     def __init__(
         self,
@@ -307,26 +96,6 @@ class RepresentationNetwork(torch.nn.Module):
         downsample,
     ):
         super().__init__()
-        self.downsample = downsample
-        if self.downsample:
-            if self.downsample == "resnet":
-                self.downsample_net = DownSample(
-                    observation_shape[0] * (stacked_observations + 1)
-                    + stacked_observations,
-                    num_channels,
-                )
-            elif self.downsample == "CNN":
-                self.downsample_net = DownsampleCNN(
-                    observation_shape[0] * (stacked_observations + 1)
-                    + stacked_observations,
-                    num_channels,
-                    (
-                        math.ceil(observation_shape[1] / 16),
-                        math.ceil(observation_shape[2] / 16),
-                    ),
-                )
-            else:
-                raise NotImplementedError('downsample should be "resnet" or "CNN".')
         self.conv = conv3x3(
             observation_shape[0] * (stacked_observations + 1) + stacked_observations,
             num_channels,
@@ -356,7 +125,6 @@ class DynamicsNetwork(torch.nn.Module):
         num_channels,
         reduced_channels_reward,
         fc_reward_layers,
-        full_support_size,
         block_output_size_reward,
     ):
         super().__init__()
@@ -371,7 +139,7 @@ class DynamicsNetwork(torch.nn.Module):
         )
         self.block_output_size_reward = block_output_size_reward
         self.fc = mlp(
-            self.block_output_size_reward, fc_reward_layers, full_support_size,
+            self.block_output_size_reward, fc_reward_layers, 1
         )
 
     def forward(self, x):
@@ -397,7 +165,6 @@ class PredictionNetwork(torch.nn.Module):
         reduced_channels_policy,
         fc_value_layers,
         fc_policy_layers,
-        full_support_size,
         block_output_size_value,
         block_output_size_policy,
     ):
@@ -411,7 +178,7 @@ class PredictionNetwork(torch.nn.Module):
         self.block_output_size_value = block_output_size_value
         self.block_output_size_policy = block_output_size_policy
         self.fc_value = mlp(
-            self.block_output_size_value, fc_value_layers, full_support_size
+            self.block_output_size_value, fc_value_layers,1 
         )
         self.fc_policy = mlp(
             self.block_output_size_policy, fc_policy_layers, action_space_size,
@@ -448,7 +215,7 @@ class MuZeroResidualNetwork(AbstractNetwork):
     ):
         super().__init__()
         self.action_space_size = action_space_size
-        self.full_support_size = 2 * support_size + 1
+
         block_output_size_reward = (
             (
                 reduced_channels_reward
@@ -495,7 +262,6 @@ class MuZeroResidualNetwork(AbstractNetwork):
                 num_channels + 1,
                 reduced_channels_reward,
                 fc_reward_layers,
-                self.full_support_size,
                 block_output_size_reward,
             )
         )
@@ -509,7 +275,6 @@ class MuZeroResidualNetwork(AbstractNetwork):
                 reduced_channels_policy,
                 fc_value_layers,
                 fc_policy_layers,
-                self.full_support_size,
                 block_output_size_value,
                 block_output_size_policy,
             )
@@ -549,23 +314,8 @@ class MuZeroResidualNetwork(AbstractNetwork):
         return encoded_state_normalized
 
     def dynamics(self, encoded_state, action):
-        # Stack encoded_state with a game specific one hot encoded action (See paper appendix Network Architecture)
-        action_one_hot = (
-            torch.ones(
-                (
-                    encoded_state.shape[0],
-                    1,
-                    encoded_state.shape[2],
-                    encoded_state.shape[3],
-                )
-            )
-            .to(action.device)
-            .float()
-        )
-        action_one_hot = (
-            action[:, :, None, None] * action_one_hot / self.action_space_size
-        )
-        x = torch.cat((encoded_state, action_one_hot), dim=1)
+        # stack action with encoded state
+        x = torch.cat((encoded_state, action), dim=1)
         next_encoded_state, reward = self.dynamics_network(x)
 
         # Scale encoded state between [0, 1] (See paper appendix Training)
@@ -598,14 +348,7 @@ class MuZeroResidualNetwork(AbstractNetwork):
         encoded_state = self.representation(observation)
         policy_logits, value = self.prediction(encoded_state)
         # reward equal to 0 for consistency
-        reward = torch.log(
-            (
-                torch.zeros(1, self.full_support_size)
-                .scatter(1, torch.tensor([[self.full_support_size // 2]]).long(), 1.0)
-                .repeat(len(observation), 1)
-                .to(observation.device)
-            )
-        )
+        reward =  0
         return (
             value,
             reward,
@@ -636,50 +379,3 @@ def mlp(
         act = activation if i < len(sizes) - 2 else output_activation
         layers += [torch.nn.Linear(sizes[i], sizes[i + 1]), act()]
     return torch.nn.Sequential(*layers)
-
-
-def support_to_scalar(logits, support_size):
-    """
-    Transform a categorical representation to a scalar
-    See paper appendix Network Architecture
-    """
-    # Decode to a scalar
-    probabilities = torch.softmax(logits, dim=1)
-    support = (
-        torch.tensor([x for x in range(-support_size, support_size + 1)])
-        .expand(probabilities.shape)
-        .float()
-        .to(device=probabilities.device)
-    )
-    x = torch.sum(support * probabilities, dim=1, keepdim=True)
-
-    # Invert the scaling (defined in https://arxiv.org/abs/1805.11593)
-    x = torch.sign(x) * (
-        ((torch.sqrt(1 + 4 * 0.001 * (torch.abs(x) + 1 + 0.001)) - 1) / (2 * 0.001))
-        ** 2
-        - 1
-    )
-    return x
-
-
-def scalar_to_support(x, support_size):
-    """
-    Transform a scalar to a categorical representation with (2 * support_size + 1) categories
-    See paper appendix Network Architecture
-    """
-    # Reduce the scale (defined in https://arxiv.org/abs/1805.11593)
-    x = torch.sign(x) * (torch.sqrt(torch.abs(x) + 1) - 1) + 0.001 * x
-
-    # Encode on a vector
-    x = torch.clamp(x, -support_size, support_size)
-    floor = x.floor()
-    prob = x - floor
-    logits = torch.zeros(x.shape[0], x.shape[1], 2 * support_size + 1).to(x.device)
-    logits.scatter_(
-        2, (floor + support_size).long().unsqueeze(-1), (1 - prob).unsqueeze(-1)
-    )
-    indexes = floor + support_size + 1
-    prob = prob.masked_fill_(2 * support_size < indexes, 0.0)
-    indexes = indexes.masked_fill_(2 * support_size < indexes, 0.0)
-    logits.scatter_(2, indexes.long().unsqueeze(-1), prob.unsqueeze(-1))
-    return logits
